8003d0b218aacc74dd229cae6fe1dbc092c1056a
Ravi Piplani <ravipiplani@outlook.com>
2025-10-01 23:59:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-900">
       <Hero />
-      <ProductShowcase />
+      <!--<ProductShowcase />-->
       <WhyRhobots />
       <WhoWeWorkWith />
       <CallToAction />
