12fe64376b209d43ff99d2d91b2c7d9eb6c61dd1
Gulshan Yadav <gulshany01@gmail.com>
2025-10-01 23:04:23 +0530

    removed products

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-900">
       <Hero />
-      <ProductShowcase />
+    
       <WhyRhobots />
       <WhoWeWorkWith />
       <CallToAction />
